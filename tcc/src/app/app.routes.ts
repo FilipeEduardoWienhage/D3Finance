a02330a6e7e0a6d6ff008c73db9ee2603d8dcaf0
Filipe Eduardo Wienhage <filipe.eduw@hotmail.com>
2024-12-24 13:12:10 -0300

    adicionandoComponentes

--- tcc/src/app/app.routes.ts
+++ tcc/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", component: HomeComponent },
+    { path: "login", component: LoginComponent},
+    { path: "cadastro", component: CadastroComponent},
     { path: "**", redirectTo: "/home" }
 ];
