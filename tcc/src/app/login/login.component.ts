f735381f2f298df63f3d9c90ff06649140b89ba2
Filipe Eduardo Wienhage <filipe.eduw@hotmail.com>
2024-12-25 20:53:52 -0300

    loginBackgtoundNavBar

--- tcc/src/app/login/login.component.ts
+++ tcc/src/app/login/login.component.ts
@@ -1,34 +1,15 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
-import { FocusTrapModule } from 'primeng/focustrap';
-import { ButtonModule } from 'primeng/button';
-import { FormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { AutoFocusModule } from 'primeng/autofocus';
-import { Card } from 'primeng/card';
+
 
 @Component({
   selector: 'app-login',
   imports: [
     NavBarComponent,
-    FocusTrapModule,
-    ButtonModule,
-    FormsModule,
-    InputTextModule,
-    IconFieldModule,
-    InputIconModule,
-    AutoFocusModule,
-    Card,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  name: string = '';
 
-    email: string = '';
-
-    accept: boolean = false;
 }
