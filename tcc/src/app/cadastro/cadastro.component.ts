4e78f86f0e2b4b39ca90cdfc0eb071525fe3e122
Luis Spezia <lhspezia777@gmail.com>
2024-12-26 17:10:35 -0300

    Adicionado cards na tela Contatos

--- tcc/src/app/cadastro/cadastro.component.ts
+++ tcc/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [NavBarComponent],
+  imports: [
+  NavBarComponent,
+  FloatLabelModule,
+  FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  nome: string;
+
+  constructor() {
+    this.nome = '';
+  }
 
 }
