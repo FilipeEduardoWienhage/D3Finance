d70b310311bd6580c67c8e463859219c8b50aad1
Filipe Eduardo Wienhage <filipe.eduw@hotmail.com>
2024-12-26 22:03:38 -0300

    footer

--- tcc/src/app/contato/contato.component.ts
+++ tcc/src/app/contato/contato.component.ts
@@ -3,10 +3,11 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import "primeicons/primeicons.css";
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-contato',
-  imports: [NavBarComponent, CardModule, ButtonModule],
+  imports: [NavBarComponent, CardModule, ButtonModule, FooterComponent],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css',
 })
