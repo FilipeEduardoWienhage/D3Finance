de79448a9de7535b4382e2e65cad5bf13e112bf0
Luis Spezia <lhspezia777@gmail.com>
2024-12-28 11:33:32 -0300

    Alinhado icones Insta/Linkedin/QR

--- tcc/src/app/contato/contato.component.ts
+++ tcc/src/app/contato/contato.component.ts
@@ -3,7 +3,6 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import "primeicons/primeicons.css";
-import { FooterComponent } from '../footer/footer.component';
 import { QRCodeComponent } from 'angularx-qrcode';
 
 
