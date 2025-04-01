fd4140999dd57b0a2de9b9b61cee81c5fab28bb2
Filipe Eduardo Wienhage <filipe.eduw@hotmail.com>
2025-04-01 19:30:45 -0300

    criado backend

--- django/backend/accounts/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class EmailConfirmationForm(forms.Form):
-    email = forms.EmailField(label="E-mail", required=True)
--- django/backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('confirm_email/', views.confirm_email, name='confirm_email'),  # pagina confirmar email
-    path('send_confirmation/', views.send_confirmation_email, name='send_confirmation'),  # formulario para enviar link
-    path('email_sent/', views.email_sent, name='email_sent'),  # pagina que informa que o email foi enviado
-    # path('email_confirmed/',views.confirm_email, name='') 
-]
