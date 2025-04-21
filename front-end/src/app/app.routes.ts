635c05fdfca354f74e633cd32929770c87d84951
Filipe Eduardo Wienhage <filipe.eduw@hotmail.com>
2025-04-21 14:40:39 -0300

    Reorganização Front-End

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
-import { LoginComponent } from './views/login/login.component';
-import { CadastroComponent } from './views/cadastro/cadastro.component';
-import { ContatoComponent } from './views/contato/contato.component';
+import { HomeComponent } from './views/public/home/home.component';
+import { LoginComponent } from './views/public/login/login.component';
+import { CadastroComponent } from './views/public/cadastro/cadastro.component';
+import { ContatoComponent } from './views/public/contato/contato.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
