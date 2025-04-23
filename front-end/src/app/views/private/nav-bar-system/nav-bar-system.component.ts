19dd529c1366b1169607382774f55355a2df01d8
Luis Spezia <lhspezia777git config --global user.email lhspezia777@gmail.com>
2025-04-23 17:07:45 -0300

    Feito modal de Logout do sistema para a tela Home

--- front-end/src/app/views/private/nav-bar-system/nav-bar-system.component.ts
+++ front-end/src/app/views/private/nav-bar-system/nav-bar-system.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { Menu } from 'primeng/menu';
+import { MenuModule } from 'primeng/menu';
 import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav-bar-system',
-  imports: [ToastModule, Menu],
+  standalone: true,
+  imports: [MenuModule, ToastModule, ConfirmDialogModule],
+  providers: [ConfirmationService],
   templateUrl: './nav-bar-system.component.html',
   styleUrl: './nav-bar-system.component.css'
 })
+
 export class NavBarSystemComponent {
   items: MenuItem[] | undefined;
+
+  constructor(
+    private confirmationService: ConfirmationService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.items = [
@@ -57,11 +68,28 @@ export class NavBarSystemComponent {
           },
           {
             label: 'Logout',
-            icon: 'pi pi-sign-out'
+            icon: 'pi pi-sign-out',
+            command: () => this.confirmLogout()
           }
         ]
       }
     ];
   }
+  confirmLogout() {
+    this.confirmationService.confirm({
+      message: 'Deseja realmente sair?',
+      header: 'Logout',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Sair',
+      rejectLabel: 'Cancelar',
+      accept: () => {
+        this.logout();
+      }
+    });
+  }
 
+  logout() {
+    localStorage.removeItem('authToken');
+    this.router.navigate(['/home']);
+  }
 }
