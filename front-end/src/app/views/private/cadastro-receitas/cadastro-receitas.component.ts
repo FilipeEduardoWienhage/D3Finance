9d689480b112045d585c4fa12b33c3a85cf817c7
FilipeEduardoWienhage <164120086+FilipeEduardoWienhage@users.noreply.github.com>
2025-04-24 00:18:11 -0300

    Necessário ajustar o menu (#41)

--- front-end/src/app/views/private/cadastro-receitas/cadastro-receitas.component.ts
+++ front-end/src/app/views/private/cadastro-receitas/cadastro-receitas.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { NavBarSystemComponent } from '../nav-bar-system/nav-bar-system.component';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { InputNumber } from 'primeng/inputnumber';
+import { Fluid } from 'primeng/fluid';
+import { DatePicker } from 'primeng/datepicker';
+import { TextareaModule } from 'primeng/textarea';
+import { SelectModule } from 'primeng/select';
+import { ButtonModule } from 'primeng/button';
+
+
+interface formaRecebimento {
+  name: string;
+}
 
 @Component({
   selector: 'app-cadastro-receitas',
-  imports: [FooterComponent, NavBarSystemComponent],
+  imports: [FooterComponent, 
+            NavBarSystemComponent, 
+            CardModule, 
+            InputTextModule, 
+            FormsModule,
+            InputNumber,
+            Fluid,
+            DatePicker,
+            TextareaModule,
+            SelectModule,
+            ButtonModule],
   templateUrl: './cadastro-receitas.component.html',
   styleUrl: './cadastro-receitas.component.css'
 })
 export class CadastroReceitasComponent {
+  nomeReceita: string = '';
+  valorReceita: number = 0;
+  dataRecebimento: Date | null = null;
+
+  formaDeRecebimento: formaRecebimento[] | undefined;
+
+    selecionarForma: formaRecebimento | undefined;
+
+    ngOnInit() {
+        this.formaDeRecebimento = [
+            { name: 'Dinheiro'},
+            { name: 'Débito'},
+            { name: 'Crédito'},
+            { name: 'Cheque'},
+            { name: 'Depósito'},
+            { name: 'Pix'}
+        ];
+    }
 
 }
