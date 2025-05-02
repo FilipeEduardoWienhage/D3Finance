26c505f417dc20eea4becd53861df19874d3301c
Filipe Eduardo Wienhage <filipe.eduw@hotmail.com>
2025-05-02 20:33:00 -0300

    Tela de cadastro de contas, requisição front-end e ajustes nos inputs

--- front-end/src/app/views/private/cadastro-contas/cadastro-contas.component.ts
+++ front-end/src/app/views/private/cadastro-contas/cadastro-contas.component.ts
@@ -3,17 +3,55 @@ import { SplitterModule } from 'primeng/splitter';
 import { NavBarSystemComponent } from '../nav-bar-system/nav-bar-system.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { MessageService } from 'primeng/api';
+import { CardModule } from 'primeng/card';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ContaRequestModel } from '../../../models/RequestContas';
+import { ContasService } from '../../../service/contas.service';
+
+
+interface tipoConta {
+  name: string;
+}
 
 
 @Component({
   selector: 'app-cadastro-contas',
-  imports: [SplitterModule, NavBarSystemComponent, FooterComponent],
+  imports: [SplitterModule, NavBarSystemComponent, FooterComponent, CardModule, SelectModule, FormsModule, ButtonModule],
   templateUrl: './cadastro-contas.component.html',
   styleUrl: './cadastro-contas.component.css',
   providers: [
-      MessageService,
-    ]
+    MessageService,
+  ]
 })
 export class CadastroContasComponent {
+  public requestConta!: ContaRequestModel;
 
+  constructor(private contasService: ContasService) {}
+
+  tipoConta: tipoConta[] | undefined;
+  selecionarTipoConta: tipoConta | undefined;
+
+  ngOnInit(): void {
+    this.requestConta = new ContaRequestModel();
+
+    this.tipoConta = [
+      { name: 'Pessoal' },
+      { name: 'Empresa' },
+      { name: 'Despesas' },
+    ];
+
+  }
+
+  public doCadastroContas(): void {
+    console.log(this.requestConta);
+    this.contasService.cadastrarConta(this.requestConta).subscribe({
+      next: () => alert("Conta cadastrada com sucesso!"),
+      error: erro => {
+        console.error(erro)
+        alert("Erro ao efetuar o cadastro!");
+      }
+    });
+  }
 }
