b75b9d6c3f8290bc58dd109330cd62a63e447f23
FilipeEduardoWienhage <164120086+FilipeEduardoWienhage@users.noreply.github.com>
2025-05-18 00:42:51 -0300

    Movimentação entre contas funcionando (#73)

--- front-end/src/app/views/private/movimentacao-entre-contas/movimentacao-entre-contas.component.ts
+++ front-end/src/app/views/private/movimentacao-entre-contas/movimentacao-entre-contas.component.ts
@@ -4,23 +4,26 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { SplitterModule } from 'primeng/splitter';
 import { MessageService } from 'primeng/api';
 import { DividerModule } from 'primeng/divider';
+import { FormsModule } from '@angular/forms';
 import { InputNumber } from 'primeng/inputnumber';
 import { Fluid } from 'primeng/fluid';
 import { SelectModule } from 'primeng/select';
 import { ContasService } from '../../../service/contas.service';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-import { Password } from 'primeng/password';
+import { TransacaoService } from '../../../service/transacao.service';
+import { TransacaoRequestModel } from '../../../models/RequestTransacao';
 
 
-interface contaDestino {
+
+interface Conta {
   id: number;
   name: string;
 }
 
 @Component({
   selector: 'app-movimentacao-entre-contas',
-  imports: [NavBarSystemComponent, FooterComponent, SplitterModule, DividerModule, InputNumber, Fluid, SelectModule, CardModule, ButtonModule],
+  imports: [NavBarSystemComponent, FooterComponent, FormsModule, SplitterModule, DividerModule, InputNumber, Fluid, SelectModule, CardModule, ButtonModule],
   templateUrl: './movimentacao-entre-contas.component.html',
   styleUrl: './movimentacao-entre-contas.component.css',
   providers: [
@@ -28,44 +31,46 @@ interface contaDestino {
   ]
 })
 export class MovimentacaoEntreContasComponent {
+  
+  transacao: TransacaoRequestModel = new TransacaoRequestModel();
+  contas: Conta[] = [];
 
-  constructor(private contasService: ContasService) { }
-
-  valorDestino: number = 0;
-  valorOrigem: number = 0;
-  contaDestino: contaDestino[] | undefined;
-  selecionarConta: contaDestino | undefined;
+  constructor(
+    private contasService: ContasService,
+    private transacaoService: TransacaoService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.contasService.listarContas().subscribe({
       next: (contas) => {
-        this.contaDestino = contas.map(conta => ({
+        this.contas = contas.map(conta => ({
           id: conta.id,
           name: conta.nome_conta
         }));
       },
-      error: (erro) => {
-        console.error('Erro ao carregar contas:', erro);
-      }
+      error: (erro) => console.error('Erro ao carregar contas:', erro)
     });
   }
 
-  public doMovimentarContas(): void {
-    
+  doMovimentarContas(): void {
+    this.transacaoService.cadastrarTransacao(this.transacao).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Transação realizada com sucesso!'
+        });
+        this.transacao = new TransacaoRequestModel();
+      },
+      error: (erro) => {
+        console.error('Erro ao movimentar contas:', erro);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao realizar movimentação. Tente novamente.'
+        });
+      }
+    });
   }
-  //   if (this.selecionarConta) {
-  //     this.selecionarConta.id;
-  //   }
-
-  //   console.log(this.);
-  //   this.receitaService.cadastrarReceita(this.requestReceita).subscribe({
-  //     next: () => alert("Receita cadastrada com sucesso!"),
-  //     error: erro => {
-  //       console.error(erro)
-  //       alert("Erro ao efetuar o cadastro!");
-  //     }
-  //   });
-  // }
-  
-
 }
