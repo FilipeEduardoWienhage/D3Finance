37c97a6f55c67b53ba9b40b2c04f5711a68fbcfd
FilipeEduardoWienhage <164120086+FilipeEduardoWienhage@users.noreply.github.com>
2025-08-30 16:34:46 -0300

    Criado o service para o front-end (#126)

---
+++ front-end/src/app/service/relatorio.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environment/environment.prod';
+
+export interface CategoriaValor {
+  categoria: string;
+  valor: number;
+}
+
+export interface DadosMensais {
+  mes: number;
+  receitas: number;
+  despesas: number;
+  saldo_periodo: number;
+}
+
+export interface RelatorioMensal {
+  periodo_inicio: string;
+  periodo_fim: string;
+  total_receitas: number;
+  total_despesas: number;
+  saldo_periodo: number;
+  receitas_por_categoria: CategoriaValor[];
+  despesas_por_categoria: CategoriaValor[];
+  quantidade_receitas: number;
+  quantidade_despesas: number;
+}
+
+export interface RelatorioAnual {
+  ano: number;
+  total_receitas: number;
+  total_despesas: number;
+  saldo_periodo: number;
+  dados_mensais: DadosMensais[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RelatorioService {
+
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  // Relatório Mensal
+  getRelatorioMensal(ano: number, mes: number): Observable<RelatorioMensal> {
+    const params = new HttpParams()
+      .set('ano', ano.toString())
+      .set('mes', mes.toString());
+
+    return this.http.get<RelatorioMensal>(`${this.apiUrl}/relatorios/mensal`, { params });
+  }
+
+  // Relatório Anual
+  getRelatorioAnual(ano: number): Observable<RelatorioAnual> {
+    const params = new HttpParams().set('ano', ano.toString());
+
+    return this.http.get<RelatorioAnual>(`${this.apiUrl}/relatorios/anual`, { params });
+  }
+}
